928be357b6685f80086570f92a645aa29cedf9ea
Beyonce Knowles <you@example.com>
2021-03-21 13:25:34 -0400

    node 14 fix

--- postgrator-config.js
+++ postgrator-config.js
@@ -2,15 +2,12 @@
 require('dotenv').config();
 
 const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production";
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
   'connectionString': (process.env.NODE_ENV === 'test' )
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
-  'ssl' : {
-    rejectedUnauthorized: false,
-  }  
+    : process.env.DATABASE_URL, 
 };
--- src/server.js
+++ src/server.js
@@ -5,8 +5,7 @@ const { PORT, DATABASE_URL } = require('./config');
 const app = require('./app');
 
 const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production";
-
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
